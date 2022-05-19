0486f61597fdf96540a2a68186f1764ad886d758
Philippe De Andria <philippedeandria12@hotmail.com>
2022-05-19 09:15:54 +0300

    baby steps

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,9 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #devise
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
