81b096bbd165e9dcd348dc76bc07a60545020bca
Philippe De Andria <philippedeandria12@hotmail.com>
2022-05-30 19:43:18 +0300

    order

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,4 @@
 class OrderItem < ApplicationRecord
+  belongs_to :OrderList
+  belongs_to :product
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
 validates :name, :price, :category, :size, presence: true
+has_many :order_items
 end
