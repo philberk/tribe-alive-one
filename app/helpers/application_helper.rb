b62a6ac20e340cd70dc5499920729de4fe0d4475
Philippe De Andria <philippedeandria12@hotmail.com>
2022-05-31 12:41:55 +0300

    controllerss

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
 
-  def current_order
-    if session[:order_id].nil?
-      OrderList.new
-    else
-      OrderList.find(session[:order_id])
-    end
-  end
+  # def current_order
+  #   if session[:order_id].nil?
+  #     OrderList.new
+  #   else
+  #     OrderList.find(session[:order_id])
+  #   end
+  # end
 end
+ 
